1ca1a400d130f6f3126015103ef4b3660f7dc453
Vinicim <vinicius_anacleto@estudante.sesisenai.org.br>
2026-03-24 14:30:14 -0300

    feat: Snake game finalization

--- Snake.cpp
+++ Snake.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <deque>
 #include <windows.h>
+#include <conio.h>
+#include <cstdlib>
 
 using namespace std;
 
@@ -15,6 +17,8 @@ struct Point { int x, y; };
 
 deque<Point> snake;
 Point food;
+
+int score = 0;
 
 void gotoxy(int x, int y) {
     COORD c = { (SHORT)x, (SHORT)y };
@@ -51,32 +55,95 @@ void draw() {
         cout << "\n";
     }
 
-    cout << "Score: 0\n";
+    cout << "Score: " << score << "\n";
 }
 
 void logic() {
     Point novasCabeca = { snake.front().x + dirX,
-                          snake.front().y + dirY};
+                          snake.front().y + dirY };
 
+    // 1. bateu na borda?
+    if (novasCabeca.x <= 0 || novasCabeca.x >= WIDTH - 1 ||
+        novasCabeca.y <= 0 || novasCabeca.y >= HEIGHT - 1) {
+        gameRunning = false;
+        return;
+    }
+
+    // 2. bateu no próprio corpo?
+    for (Point p : snake) {
+        if (p.x == novasCabeca.x && p.y == novasCabeca.y) {
+            gameRunning = false;
+            return;
+        }
+    }
+
+    // 3. move
     snake.push_front(novasCabeca);
-    snake.pop_back();
+
+    // 4. comeu a comida?
+    if (novasCabeca.x == food.x && novasCabeca.y == food.y) {
+        score++;
+        food = { rand() % (WIDTH - 2) + 1,
+                 rand() % (HEIGHT - 2) + 1 };
+    } else {
+        snake.pop_back();
+    }
+}
+
+void input() {
+    if (!_kbhit()) return;
+    char ch = _getch();
+
+    switch(ch) {
+        case 'w': case 'W': if (dirY != 1) { dirX = 0; dirY = -1; } break;
+        case 's': case 'S': if (dirY != -1) { dirX = 0; dirY = 1; } break;
+        case 'a': case 'A': if (dirX != 1) { dirX = -1; dirY = 0; } break;
+        case 'd': case 'D': if (dirX != -1) { dirX = 1; dirY = 0; } break;
+        case 'x': case 'X': gameRunning = false; break;
+    }
+}
+
+void setup() {
+    snake.clear();
+    snake.push_back({5, 3});
+    snake.push_back({4, 3});
+    snake.push_back({3, 3});
+
+    food = { rand() % (WIDTH - 2) + 1,
+         rand() % (HEIGHT - 2) + 1};
+
+    dirX = 1;
+    dirY = 0;
+    score = 0;
+    gameRunning = true;
 }
 
 int main () {
     hideCursor();
     system("cls");
+    srand((unsigned)time(nullptr));
     
-    snake.push_back({5,3});
-    snake.push_back({4,3});
-    snake.push_back({3,3});
+    setup();
 
-    food = {10, 5};
+    while (true) {
+        while(gameRunning) {
+            draw();
+            input();
+            logic();
+            Sleep(150);
+        }
 
-    while(true) {
-        draw();
-        logic();
-        Sleep(200);
+        gotoxy(0, HEIGHT + 1);
+        cout << "Game over! Score " << score << "      \n";
+        cout << "Jogar novamente? (S/N): ";
+
+        char ch = _getch();
+        if (ch == 's' || ch == 'S') {
+            system("cls");
+            setup();
+        } else {
+            break;
+        }
     }
-    
     return 0;
 }
